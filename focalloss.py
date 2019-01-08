f855cac9f7f7b463422fc4501fe5c6562bf34c78
k_muta <muta@m.cs.osakafu-u.ac.jp>
2019-01-08 21:02:15 +0100

    delete alpha

--- focalloss.py
+++ focalloss.py
@@ -8,8 +8,8 @@ class FocalLoss(nn.Module):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
         self.alpha = alpha
-        if isinstance(alpha,(float,int,long)): self.alpha = torch.Tensor([alpha,1-alpha])
-        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
+        '''if isinstance(alpha,(float,int,long)): self.alpha = torch.Tensor([alpha,1-alpha])
+        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)'''
         self.size_average = size_average
 
     def forward(self, input, target):
@@ -24,11 +24,11 @@ class FocalLoss(nn.Module):
         logpt = logpt.view(-1)
         pt = Variable(logpt.data.exp())
 
-        if self.alpha is not None:
+        '''if self.alpha is not None:
             if self.alpha.type()!=input.data.type():
                 self.alpha = self.alpha.type_as(input.data)
             at = self.alpha.gather(0,target.data.view(-1))
-            logpt = logpt * Variable(at)
+            logpt = logpt * Variable(at)'''
 
         loss = -1 * (1-pt)**self.gamma * logpt
         if self.size_average: return loss.mean()
