b09a1963a6f0ceeee1f539e31643431866ad2ddc
k_muta <muta@m.cs.osakafu-u.ac.jp>
2019-01-09 01:45:30 +0100

    add CAM

--- train_ISIC2016.py
+++ train_ISIC2016.py
@@ -20,13 +20,13 @@ from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import auc, roc_auc_score
-from sklearn.metrics import roc_curve
+from sklearn.metrics import auc, roc_curve
 from enum import Enum
 # Custom imports
 import pretrainedmodels
 from cutout import *
 from focalloss import *
+import cv2
 #from se_resnext import *
 import matplotlib
 matplotlib.use('Agg')
@@ -160,6 +160,7 @@ def train(options):
             for name2, params in child.named_parameters():
                 print(name, name2)
 
+        finalconv_name = 'features'
         ## Change the last layer
         inputDim = model.classifier.in_features
         model.classifier = torch.nn.Linear(inputDim, options.numClasses)
@@ -311,7 +312,13 @@ def train(options):
     correctExamples = 0
     oneHot = []
     pred_fold = []
+    features_blobs = []
     model.eval()
+    #hooked feature map
+    model._modules.get(finalconv_name).register_forward_hook(hook_feature)
+    # get the softmax weight
+    params = list(model.parameters())
+    weight_softmax = np.squeeze(params[-2].data.numpy())
     for iterationIdx, data in enumerate(dataLoaderVal):
         X = data["data"]
         y = data["label"]
@@ -389,11 +396,38 @@ def train(options):
         for idx in range(len(predictedLabels)):
             print(predictedLabels[idx], file=predchecking)
 
+    # generate class activation mapping for the top1 prediction
+    print('feature_blob', features_blobs[0])
+    CAMs = returnCAM(features_blobs[0], weight_softmax, 0)
+
+    # render the CAM and output
+    img = cv2.imread('2016test/ISIC_0000003.jpg')
+    height, width, _ = img.shape
+    heatmap = cv2.applyColorMap(cv2.resize(CAMs[0],(width, height)), cv2.COLORMAP_JET)
+    result = heatmap * 0.3 + img * 0.5
+    cv2.imwrite('CAM.jpg', result)
+
     predictedLabels.clear()
     gtLabels.clear()
     plbs.clear()
     glbs.clear()
 
+def hook_feature(module, input, output):
+    features_blobs.append(output.data.cpu().numpy())
+
+def returnCAM(feature_conv, weight_softmax, class_idx):
+    # generate the class activation maps upsample to 256x256
+    size_upsample = (256, 256)
+    bz, nc, h, w = feature_conv.shape
+    output_cam = []
+    for idx in class_idx:
+        cam = weight_softmax[idx].dot(feature_conv.reshape((nc, h*w)))
+        cam = cam.reshape(h, w)
+        cam = cam - np.min(cam)
+        cam_img = cam / np.max(cam)
+        cam_img = np.uint8(255 * cam_img)
+        output_cam.append(cv2.resize(cam_img, size_upsample))
+    return output_cam
 
 def plot(options):
     tloss_x = []
