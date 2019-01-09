dd1ca77d6744115858ea6e74a7104b44d1a46c07
k_muta <muta@m.cs.osakafu-u.ac.jp>
2019-01-09 23:51:51 +0100

    NTS-Net

---
+++ NTS-Net/config.py
@@ -0,0 +1,10 @@
+BATCH_SIZE = 16
+PROPOSAL_NUM = 6
+CAT_NUM = 4
+INPUT_SIZE = (224, 224)  # (w, h)
+LR = 0.0001
+WD = 1e-4
+SAVE_FREQ = 1
+resume = ''
+test_model = 'model.ckpt'
+save_dir = 'NTS_model'
